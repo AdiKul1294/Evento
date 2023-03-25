9948b81d92d21bb572f998993c072b89376f729e
AdiKul1294 <f20211294@goa.bits-pilani.ac.in>
2023-03-25 22:44:25 +0530

    Added animation and changed background in Pass & Login Screens

--- app/src/main/java/com/devlite/evento/fragments/PassFragment.kt
+++ app/src/main/java/com/devlite/evento/fragments/PassFragment.kt
@@ -5,16 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.databinding.DataBindingUtil
 import com.devlite.evento.R
+import com.devlite.evento.databinding.FragmentPassBinding
 
 class PassFragment : Fragment() {
-
+    private lateinit var binding: FragmentPassBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pass, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_pass, container, false)
+
+        binding.titleTvPf.startAnimation(AnimationUtils.loadAnimation(context, R.anim.slide_in))
+        binding.eventsTvPf.startAnimation(AnimationUtils.loadAnimation(context, R.anim.slide_in));
+
+        return binding.root
     }
 
 }
